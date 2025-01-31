65ba4758a5854a5de50afa2c2592aa10722acf68
Rob Mills <robcmills@gmail.com>
2025-01-31 08:59:15 -0800

    Implement character_namer scene

---
+++ scripts/character_namer.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var line_edit: LineEdit = $VBoxContainer/CenterContainer2/LineEdit
+@onready var character: HippoCharacter = $VBoxContainer/CenterContainer3/Control/HippoPlayer
+
+var character_name := ""
+var character_preset := Enums.CharacterPreset.HUMAN
+
+
+func _ready() -> void:
+	line_edit.grab_focus()
+	character.characterPreset = character_preset
+	character.animation = Enums.PlayerAnimation.READY
+	character.playAnimation()
+
+
+func _on_line_edit_text_changed(new_text: String) -> void:
+	character_name = line_edit.text
+	var text_length = line_edit.text.length()
+	var width = 420 if text_length == 0 else 64 + text_length * 24
+	line_edit.custom_minimum_size = Vector2(width, 72)
+
+
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	print("Hello, " + new_text)
