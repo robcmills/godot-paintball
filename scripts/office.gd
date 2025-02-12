166c618bf9c09355c23455c4ecfed467200d7d70
Rob Mills <robcmills@gmail.com>
2025-02-11 21:15:43 -0800

    Fix y-sort issue for hippo character

--- scripts/office.gd
+++ scripts/office.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var viewport: SubViewport = $SubViewport
-@onready var splat_sprite: Sprite2D = $SplatSprite
+#@onready var viewport: SubViewport = $SubViewport
+#@onready var splat_sprite: Sprite2D = $SplatSprite
 
-func _ready() -> void:
-	splat_sprite.material.set_shader_parameter("mask_tex", viewport.get_texture())
+#func _ready() -> void:
+	#splat_sprite.material.set_shader_parameter("mask_tex", viewport.get_texture())
